d47372cb95fdf49caf8dcd2e7d38252a8ae05aa8
Michel González <michiwoo.web@gmail.com>
2017-11-17 02:32:29 -0600

    15. Empezando la pantalla de signin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,16 @@ import 'hammerjs';
 
 import { QuestionDetailComponent } from './question/question-detail.component';
 import { AnswerFormComponent } from './answer/answer-form.component';
+import { SigninScreenComponent } from './auth/signin-screen.component';
+
 import { MomentModule } from 'angular2-moment';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuestionDetailComponent,
-    AnswerFormComponent
+    AnswerFormComponent,
+    SigninScreenComponent
   ],
   imports: [
     BrowserModule,
