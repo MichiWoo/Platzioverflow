0357a42fbb1d3c3edc8afb4e3c69a5cc4f04e2f3
Michel González <michiwoo.web@gmail.com>
2017-11-18 00:27:41 -0600

    19. Creando el componente para los listados de preguntas

--- src/app/question/question-list.component.ts
+++ src/app/question/question-list.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { Question } from './question.model';
+
+const q = new Question(
+  '¿Comó reciclo un componente de android?',
+  'Miren, esta es mi pregunta..',
+  new Date(),
+  'devicon-android-plain'
+);
 
 @Component({
   selector: 'app-question-list',
-  templateUrl: './question-list.component.html'
+  templateUrl: './question-list.component.html',
+  styles: [`
+    i {
+      font-size: 48px;
+    }
+    i.help {
+      width: 48px !important;
+      height: 48px !important;
+      padding: 0 !important;
+      font-size: 48px !important;
+    }
+  `]
 })
 
 export class QuestionListComponent {
+  questions: Question[] = new Array(10).fill(q);
 }
