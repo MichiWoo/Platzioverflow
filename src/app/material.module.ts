bdf526a2f46ac9db28d664a522cfdff5b614d169
Michel González <michiwoo.web@gmail.com>
2017-11-23 18:53:03 -0600

    37. Ya funcionó Obtener datos del Json.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatRadioModule } from '@angular/material/radio';
+import {
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatButtonModule,
+  MatInputModule,
+  MatListModule,
+  MatGridListModule,
+  MatRadioModule,
+  MatProgressSpinnerModule
+} from '@angular/material';
 
 const modules = [
   MatToolbarModule,
@@ -16,7 +19,8 @@ const modules = [
   MatInputModule,
   MatListModule,
   MatGridListModule,
-  MatRadioModule
+  MatRadioModule,
+  MatProgressSpinnerModule
 ];
 
 @NgModule({
