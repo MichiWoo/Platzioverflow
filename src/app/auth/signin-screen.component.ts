04bbe25afba14685a5c7648c70594335ea6b7ec8
Michel González <michiwoo.web@gmail.com>
2017-11-26 16:28:38 -0600

    50. Conectando el signin con el Backend

--- src/app/auth/signin-screen.component.ts
+++ src/app/auth/signin-screen.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { User } from './user.model';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-signin-screen',
   templateUrl: './signin-screen.component.html'
 })
-
 export class SigninScreenComponent implements OnInit {
 
   signinForm: FormGroup;
+
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
     this.signinForm = new FormGroup({
@@ -25,7 +27,12 @@ export class SigninScreenComponent implements OnInit {
     if (this.signinForm.valid) {
       const { email, password } = this.signinForm.value;
       const user = new User(email, password);
-      console.log(user);
+      this.authService.signin(user)
+        .subscribe(
+          this.authService.login,
+          err => console.log(err)
+        );
     }
   }
+
 }
